198250542717ce6be4867a4e487ba80730bd9ee9
Jingcheng Yang <yjcyxky@163.com>
2024-03-05 19:48:41 -0500

    Update the help msg for the upload component.

--- src/components/TransferTable/index.tsx
+++ src/components/TransferTable/index.tsx
@@ -1,12 +1,56 @@
 import React, { useState } from 'react';
-import { Table, Tag, Transfer, Button, Row, message } from 'antd';
+import { Table, Tag, Transfer, Button, Row, message, Tooltip } from 'antd';
 import type { ColumnsType, TableRowSelection } from 'antd/es/table/interface';
 import type { TransferProps } from 'antd/es/transfer';
 import difference from 'lodash/difference';
 import filter from 'lodash/filter';
+import { QuestionCircleOutlined } from '@ant-design/icons';
+import enUslocale from 'antd/es/locale/en_US';
 import { NodesSearchObjectClass, type DataType, type TransferTableProps } from './index.t';
 
 import './index.less';
+
+const helpMsg = (
+  <span>
+    <span>
+      The example CSV file contains two columns: node_id and node_type, representing the node's ID
+      and type, respectively. Entries are comma-separated.
+    </span>
+    <br />
+    <br />
+    <span>
+      node_id: Unique identifier for the node, prefixed to indicate its database source(e.g., MESH,
+      ENTREZ).
+    </span>
+    <br />
+    <span>
+      node_type: Category of the node, such as Disease, Gene, Compound, Symptom, Cellular Process,
+      Molecular Function, Biological Process, Pathway, or Anatomy.
+    </span>
+    <br />
+    <br />
+    <span>Prefix and Node Type Association:</span>
+    <ul>
+      <li>
+        Disease: Prefixed by MESH or MONDO, such as MESH:D002289, MESH:D015673, MONDO:0007256.
+      </li>
+      <li>Gene: Prefixed by ENTREZ, such as ENTREZ:1, ENTREZ:2.</li>
+      <li>
+        Pathway: Prefixed by KEGG, Reactome, or WikiPathways, such as KEGG:hsa04110,
+        Reactome:R-HSA-109581, WikiPathways:WP254.
+      </li>
+      <li>Symptom: Prefixed by MESH or SYMP, such as MESH:D001523, SYMP:0000001.</li>
+      <li>
+        MolecularFunction, BiologicalProcess, CellularProcess: Prefixed by GO, such as GO:0003700,
+        GO:0008150, GO:0009987.
+      </li>
+      <li>Compound: Prefixed by DrugBank or MESH, such as DrugBank:DB00001, MESH:C0006142.</li>
+    </ul>
+    <span>
+      Each node_id must have a prefix that matches its node_type for clear categorization.
+    </span>
+  </span>
+);
 
 const exampleData = [
   {
@@ -16,6 +60,22 @@ const exampleData = [
   {
     node_id: 'MESH:D015673',
     node_type: 'Disease',
+  },
+  {
+    node_id: 'ENTREZ:1',
+    node_type: 'Gene',
+  },
+  {
+    node_id: 'DrugBank:DB00001',
+    node_type: 'Compound',
+  },
+  {
+    node_id: 'MESH:C0006142',
+    node_type: 'Compound',
+  },
+  {
+    node_id: 'KEGG:hsa04110',
+    node_type: 'Pathway',
   },
 ];
 
@@ -119,6 +179,7 @@ const TableTransfer = ({ leftColumns, rightColumns, ...restProps }: TableTransfe
           columns={columns}
           dataSource={filteredItems}
           size="small"
+          locale={enUslocale.Table}
           style={{ pointerEvents: listDisabled ? 'none' : undefined }}
           onRow={({ key, disabled: itemDisabled }) => ({
             onClick: () => {
@@ -166,6 +227,29 @@ const TransferTable: React.FC<TransferTableProps> = (props) => {
 
   return (
     <Row className="transfer-table">
+      <span
+        style={{
+          background: '#d9d9d9',
+          padding: '5px',
+          border: '1px solid #d9d9d9',
+          borderRadius: '5px',
+          marginBottom: '5px',
+        }}
+      >
+        NOTE: Please follow the example file and help message to upload your data. You might get
+        them from DEGs, Publications, or other sources.
+        <Tooltip
+          title={helpMsg}
+          overlayClassName="transfer-table-help"
+          getPopupContainer={(triggerNode) => {
+            return triggerNode.parentElement || document.body;
+          }}
+        >
+          <a>
+            <QuestionCircleOutlined style={{ marginLeft: '5px' }} /> Help
+          </a>
+        </Tooltip>
+      </span>
       <TableTransfer
         dataSource={props.dataSource}
         targetKeys={targetKeys}
